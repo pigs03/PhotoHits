1f00b70da9d29e7637adc72ef123e2dd2e718e0a
pigs <gtm486@uqmobile.jp>
2020-11-19 19:04:06 +0900

    first github

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PhotoHits</title>
+</head>
+<body>
+  <header>
+    <h1>PhotoHits</h1>
+    <ul>
+      <li>MyPage</li>
+      <li>ランキング</li>
+      <li>カテゴリー</li>
+      <li>ログイン</li>
+      <li>ログイン状態</li>
+      <li>三</li>
+    </ul>
+  </header>
+  <img src="image/paris-3296269_1280.jpg" alt="title画像">
+  <h2>PhotoHits</h2>
+  <p>最初の見出し</p>
+  <form action="#" method="POST">
+    <input type="text" name="searchform" id="searchform" value="">
+  </form>
+</body>
+</html>
