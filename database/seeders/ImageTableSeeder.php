ef9dada2aae5526da8262ad6c9a3cc7d2a767232
pigs <gtm486@uqmobile.jp>
2020-11-26 23:42:08 +0900

    seeder Factory作成

---
+++ database/seeders/ImageTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use app\Models\Image;
+use Illuminate\Database\Seeder;
+
+class ImageTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
