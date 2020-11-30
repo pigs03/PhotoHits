39250f5fd26179eaee7ffe4dfad129dcfb91dc37
pigs <gtm486@uqmobile.jp>
2020-11-30 17:40:31 +0900

    20201130

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -21,8 +21,9 @@ class ImageController extends Controller
     //画像登録
     public function exeStore(ImageRequest $request) {
       $file_data = $request->except('img');
-     $file_path = $request->file('img')->store('images/');
-    Image::create($file_data, $file_path);
+     $file_path = $request->file('img');
+    dd($file_path);
+    /*Image::create($file_data, $file_path);*/
     /*$inputs = $request->all();
     Image::create($inputs);*/
     }
